f5e589f1b142dd607c364e3a7064bd4e28f983a4
Aida Fitriya <aidafitriya1206@gmail.com>
2022-12-24 16:56:05 +0700

    php dasar 1

---
+++ php_dasar1.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = "Aida Fitriya";
+$b = 20;
+$c = 29;
+$d = $c - $b;
+
+echo "Belajar PHP Dasar 1 <br>";
+echo "Hello, Nama saya <strong>$a</strong>, saya berusia <strong>$b</strong> tahun. <br>";
+echo "Selisih umur saya dengan kakak adalah = $d tahun.";
